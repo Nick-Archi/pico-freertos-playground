fe0940aa51413fec8a20ba104bd9bef67688dcee
Nick-Archi <nicholas.archibong@gmail.com>
2025-02-20 16:34:36 -0600

    Playing around with LEDs and scheduling tasks

---
+++ 01_task_priority/src/blink_tasks.c
@@ -0,0 +1,63 @@
+/*
+* Objective: Wanted to witness the scheduler performing round robin
+* with LEDs connected to the gpio ports on the RP2350.
+*/
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+#define DELAY (1500000000)
+#define LED_ON (1)
+#define LED_OFF (0)
+
+typedef struct task_data_t
+{
+    int delay;
+    int task_no;
+    int gpio_pin;
+}task_data;
+
+static task_data task1 = 
+{
+    .delay = 1000,
+    .task_no = 1,
+    .gpio_pin = 21 
+};
+
+static task_data task2 = 
+{
+    .delay = 10000,
+    .task_no = 2,
+    .gpio_pin = 22 
+};
+
+void vTaskImpl(void* pvPara)
+{
+    task_data* task = (task_data*)pvPara;
+    gpio_init(task->gpio_pin);
+    gpio_set_dir(task->gpio_pin, GPIO_OUT);
+
+    while(true)
+    {
+        gpio_put(task->gpio_pin, LED_ON);
+        for(unsigned long i = 0; i < 1000000; i++);
+        gpio_put(task->gpio_pin, LED_OFF);
+        vTaskDelay(pdMS_TO_TICKS(task->delay));
+    } 
+}
+
+int main()
+{
+    stdio_init_all();
+
+    // create task with (function addr, string, stack size, , priority lvl, )
+    xTaskCreate(vTaskImpl, "Task 1", 256, &task1, 1, NULL);
+    xTaskCreate(vTaskImpl, "Task 2", 256, &task2, 1, NULL);
+
+    vTaskStartScheduler();
+
+    while(1){};
+}
---
+++ 01_task_priority/src/main2.c
@@ -0,0 +1,51 @@
+/*
+* Objective:
+* [] showcase task starvation
+* 
+* In this example, task 2 is going to monapolize the scheduler and starve
+* out task 1. Task 1 will get to execute everynowandthen but since task 2
+* is higher priority and a lower delay, when it's ready to execute, it's going 
+* to take precedence.
+*/
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+#define DELAY (15000000)
+
+typedef struct task_data_t
+{
+    char task_number;
+    int delay;
+}task_data;
+
+task_data task_data1 = {.task_number = '1', .delay = 10};
+task_data task_data2 = {.task_number = '2', .delay = 2};
+
+void vTaskImpl(void* pvPara)
+{
+    while(true)
+    {
+        printf("Task %c is currently running\n", ((task_data*)pvPara)->task_number);
+        
+        // introduce small delay
+        //for(int i = 0; i < ((task_data*)pvPara)->delay; i++);
+        vTaskDelay(pdMS_TO_TICKS(((task_data*)pvPara)->delay));
+    }
+}
+
+int main()
+{
+    stdio_init_all();
+
+    // create task with (function addr, string, stack size, , priority lvl, )
+    xTaskCreate(vTaskImpl, "Task 1", 256, &task_data1, 1, NULL);
+    xTaskCreate(vTaskImpl, "Task 2", 256, &task_data2, 2, NULL);
+
+    vTaskStartScheduler();
+
+    while(1){};
+}
