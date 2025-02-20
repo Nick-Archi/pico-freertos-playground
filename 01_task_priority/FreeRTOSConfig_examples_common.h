69742ad7c1746896eaf7656018d4d3f2746052ca
Nick-Archi <nicholas.archibong@gmail.com>
2025-02-20 16:35:52 -0600

    Changed tick rate and max priorities

--- 01_task_priority/FreeRTOSConfig_examples_common.h
+++ 01_task_priority/FreeRTOSConfig_examples_common.h
@@ -45,8 +45,8 @@
 #define configUSE_TICKLESS_IDLE                 0
 #define configUSE_IDLE_HOOK                     0
 #define configUSE_TICK_HOOK                     0
-#define configTICK_RATE_HZ                      ( ( TickType_t ) 1000 )
-#define configMAX_PRIORITIES                    2
+#define configTICK_RATE_HZ                      ( ( TickType_t ) 100 )
+#define configMAX_PRIORITIES                    3
 #define configMINIMAL_STACK_SIZE                ( configSTACK_DEPTH_TYPE ) 512 
 #define configUSE_16_BIT_TICKS                  0
 
