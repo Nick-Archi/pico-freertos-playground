30aba1c4da55edf303138e6662d1b0d9890cde35
Nick-Archi <nicholas.archibong@gmail.com>
2025-08-01 20:36:32 -0500

    SH1106 Update on Button Press functions
    [TODO]
    - I've noticed that while the OLED is able to properly capture the button press, it seems to update slowly. May need to play around with the delays...I know I have to go back and update the SBM library too in order to make things faster?

---
+++ 04_isrs/README.md
@@ -0,0 +1,70 @@
+# FreeRTOS Pico Program 04: ISRs & Timers
+
+## Table of Content
+[Overivew](#overview)<br>
+[Objective](#objective)<br>
+[Building](#building)<br>
+[Executing](#executing)<br>
+[Useful](#useful)<br>
+[Learnings](#learnings)<br>
+
+## Overview
+ISRs are used to handle Interrupts that occur on the CPU. Using the pico-sdk, I will showcase how to interrupt on different events to complete tasks.<br>
+
+Timers are used to periodically execute a callback function. I used it to implement a button de-bouncer logic.<br>
+ 
+## Objective
+- [] Button pressed triggers ISR which updates the SH1106 
+- [] Button press triggers ISR which creates semaphore that task to update SH1106 is waiting on
+- [] Two tasks can access SH1106 to update but one increments because of button press
+
+## Setup
+Modify the FreeRTOSConfig.h:
+- Update the configMAX_PRIORITES
+- Enable Time Slicing
+    - update configUSE_TIME_SLICING = 1
+- Enable indefinite waits
+    - INCLUDE_vTaskSuspend = 1
+- Enable Timers
+    - configUSE_TIMERS = 1
+- Change Timer priority
+    - configTIMER_TASK_PRIORITY
+
+## Building
+
+Navigate to build dir and execute cmake & make
+```
+	mkdir build
+	cd build
+	cmake .. && make
+	
+	# For generating build logs for debugging...
+	# cmake --trace-expand .. 2>&1 | tee cmake_build_output.txt && make 2>&1 | tee make_build_output.txt
+```
+
+Flash the built .uf2 file onto the Raspberry Pi Pico
+```
+	# connect the pico2 via USB
+	# Drag & drop the .uf2 file into the location of where the Pico2 was connected
+	OR
+	$ cp <filename>.uf2 <dir_location>/RP2350
+```
+
+## Useful
+- View logs from the Kernel to see USB connections
+```
+	$ watch -n 1 "sudo dmesg" | tail -n 20"
+```
+- Open Minicom to see serial 
+```
+	$ watch -n 1 "sudo dmesg" | tail -n 20"
+```
+
+## Learnings
+```
+- [] Tying IRQ with ISR callback function (pico-sdk & FreeRTOS)
+- [] Executing ISR because of a hardware event
+- [] Deferring processing of ISR to a Task
+- [] Giving binary semaphore
+- [] Utilizing FreeRTOS Timer to create button de-bouncing logic
+```
---
+++ 04_isrs/src/01_isr.c
@@ -0,0 +1,167 @@
+/*
+* Objective:
+* Application that upon pressing a button will update the # of button
+* presses and relay this to the SH1106 OLED to print. Button debouncer
+* is used to capture the button presses correctly 
+*
+* Takeaways:
+* [] configurating GPIO pins to see interrupt
+* [] ISR callback from GPIO IRQ of Falling Edge
+* [] using timer to unblock semaphore for task (de-bounce logic)
+* [] task samples the gpio pin after some time (de-bounce logic)
+*/
+
+#define DBG 0
+#define DEBOUNCE_MS 50
+
+// C libraries
+#include <stdio.h>
+#include <stdlib.h>
+
+// FreeRTOS libraries
+#include "FreeRTOS.h"
+#include "semphr.h"
+#include "task.h"
+#include "timers.h"
+
+// Pico SDK Libraries
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+#include "hardware/gpio.h"
+
+// sbm includes
+#include "SH1106_Interactions.h"
+
+SemaphoreHandle_t sema;
+TimerHandle_t h_debounce;
+
+// structure to keep track of button state
+typedef struct button_state_t
+{
+    uint8_t gpio;
+    uint8_t state;
+}button_state;
+
+// task structure to keep track of OLED configuration
+typedef struct oled_config_t
+{
+    int task;
+    int page;
+    uint8_t val;
+    uint16_t ticks; 
+}oled_config;
+
+// instantiate objects for each task
+static oled_config task1;
+
+// led object
+static button_state button1;
+
+static void setupOledConfig(oled_config* dst, oled_config src);
+static void setupButtonConfig(button_state* dst, button_state src);
+
+void vOledTask(void* pvParameters);
+void vButtonHandlerISR();
+void vSampleButtonTimer();
+
+// task waiting on binarySemaphore from ISR
+void vOledTask(void* pvParameters)
+{
+    oled_config* config = (oled_config*)pvParameters;
+    char buffer1[7];
+    char buffer2[5];
+    uint16_t presses = 0;
+    while(true)
+    {
+        xSemaphoreTake(sema, portMAX_DELAY);
+//        int size1 = sprintf(buffer1, "Task %d", config->task);
+        // use sprintf to update a buffer of 0000 with the number
+        int size2 = sprintf(buffer2, "%04d", config->val);
+
+//        write_string(buffer1, config->page, 0, size1 * 8);
+        write_string(buffer2, config->page+1, 0, size2 * 8);
+        update_sh1106();
+        config->val++;
+#ifdef DBG
+printf("Presses: %d\n", ++presses);
+#endif
+        gpio_set_irq_enabled(button1.gpio, GPIO_IRQ_EDGE_FALL, true);
+    }
+}
+
+void vButtonHandlerISR()
+{
+    gpio_set_irq_enabled(button1.gpio, GPIO_IRQ_EDGE_FALL, false);
+    xTimerStartFromISR(h_debounce, 0);
+}
+
+void setup_oled(void)
+{
+    init_SH1106(
+        POCI,
+        RST,
+        CS,
+        PICO,
+        CLK
+    );
+
+    begin_sh1106();
+    set_buffer();
+    clear_buffer();
+}
+
+int main()
+{
+    // using compand literals
+    setupOledConfig(&task1, (oled_config){.task = 1, .page = 3, .val = 0, .ticks=1000});
+    setupButtonConfig(&button1, (button_state){.gpio = 15});
+
+    // setup GPIO pins
+    gpio_init(button1.gpio);
+    gpio_set_dir(button1.gpio, GPIO_IN);
+    gpio_pull_up(button1.gpio);
+
+    stdio_init_all();
+    setup_oled();
+#ifdef DBG
+sleep_ms(10000); //[TODO] give me time to connect via minicom
+printf("starting...\n");
+#endif
+    sema = xSemaphoreCreateBinary();
+    h_debounce = xTimerCreate("Button_Debounce_Timer",
+                              pdMS_TO_TICKS(DEBOUNCE_MS),
+                              pdFALSE,
+                              NULL,
+                              vSampleButtonTimer);
+
+    if(sema != NULL && h_debounce != NULL)
+    {
+        gpio_set_irq_enabled_with_callback(button1.gpio, GPIO_IRQ_EDGE_FALL, true, vButtonHandlerISR); 
+
+        xTaskCreate(vOledTask, "Button_Task1", 256, &task1, 1, NULL); 
+        vTaskStartScheduler(); 
+    }
+
+    while(1){};
+
+    return 0;
+}
+
+void vSampleButtonTimer()
+{
+    xSemaphoreGive(sema);
+}
+
+static void setupOledConfig(oled_config* dst, oled_config src)
+{
+    dst->task = src.task;
+    dst->page = src.page;
+    dst->val = src.val;
+    dst->ticks = src.ticks;
+}
+
+static void setupButtonConfig(button_state* dst, button_state src)
+{
+    dst->gpio = src.gpio;
+    dst->state = src.state;
+}
